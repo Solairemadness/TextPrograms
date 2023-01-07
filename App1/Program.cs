138eba002090dd6655cb0f85c1f16bf0951cf52d
Solairemadness <121825197+Solairemadness@users.noreply.github.com>
2023-01-07 17:29:43 +0200

    Add files via upload

---
+++ App1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! How can i call you:");
+            string? name = Console.ReadLine();
+            Console.Write("ohh, really, hello ");
+            Console.WriteLine(name);
+            Console.ReadKey();
+            Console.WriteLine("how are u today?");
+            Console.ReadLine();
+            Console.WriteLine("ok, then ");
+            Console.ReadKey();
+            Console.WriteLine("let's work together :]");
+        }
+    }
+}
